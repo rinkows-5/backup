6aeeca8f3df450b88ba9c81f2d4bd5cb038aba74
rinkows-5 <cc30.tm24@docomo.ne.jp>
2017-12-19 16:24:54 +0900

    12/19

---
+++ javascript/script10.js
@@ -0,0 +1,6 @@
+function goukei(otona,kodomo){
+    return　otona*500+kodomo+200+"円です。";
+}
+document.write(goukei(2,4)+"<br>");
+document.write(goukei(1,5)+"<br>");
+document.write(goukei(3,7)+"<br>");
---
+++ script.js
@@ -0,0 +1,3 @@
+var myoji=["田中","高橋","斉藤"];
+myoji[3]="鈴木";
+document.write(myoji);
